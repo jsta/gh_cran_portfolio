84a5f4fa0d4f47bcfe19fd593ef9093b48f76734
jsta <stachel2@msu.edu>
2019-02-03 13:21:20 -0500

    beyond simple text svgs

---
+++ hexsticker.R
@@ -0,0 +1,5 @@
+library(hexSticker)
+sticker("https://owi.usgs.gov/R/img/usgs-r-logo.png", 
+        package = "smwrWQ", h_fill = "#223974", 
+        p_size = 7, 
+        s_x = 1, s_width = 0.5, filename = "test.svg")
