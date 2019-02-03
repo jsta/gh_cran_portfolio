21d2ffcd9f07534cc75e1e7b8f73ae5c48072d48
jsta <stachel2@msu.edu>
2019-02-03 13:21:43 -0500

    create toml categories manually?

--- svg.R
+++ svg.R
@@ -1,9 +1,11 @@
+# Rscript svg.R my_project https://github.com video 
+
 cmdargs <- commandArgs(trailingOnly = TRUE)
 
 project_name <- cmdargs[1]
-project_tag  <- cmdargs[2]
-project_icon <- cmdargs[3]
-project_url  <- cmdargs[4]
+project_url  <- cmdargs[2]
+project_tag  <- cmdargs[3]
+# project_icon <- cmdargs[3]
 
 library(svglite)
 library(magrittr)
@@ -13,6 +15,7 @@ library(rsvg)
 library(reticulate)
 toml <- import("toml")
 py   <- import_builtins()
+pyyaml <- import("yaml")
 
 library(yaml)
 
@@ -25,24 +28,35 @@ dev.off()
 
 image_read_svg(outpath) %>%
   image_trim() %>%
+  image_transparent(color = "white") %>%
   image_write(outpath, format = "svg")
 
-config      <- toml$load("config.toml")
-config_list <- config$params$nav
-config_list <- c(config_list, list(list(name = project_name, 
-                                        tag = project_tag, 
-                                        icon = project_icon)))
-config$params$nav <- config_list
-
-with(py$open("config.toml", "w") %as% file, {
-  toml$dump(config, file)
-})
+# add to toml manually
+# config      <- toml$load("config.toml")
+# config_list <- config$params$nav
+# config_list <- c(config_list, list(list(name = project_name, 
+#                                         tag = project_tag, 
+#                                         icon = project_icon)))
+# config$params$nav <- config_list
+# 
+# with(py$open("config.toml", "w") %as% file, {
+#   toml$dump(config, file)
+# })
 
 yl <- yaml::read_yaml("data/links.yml")
-yl <- c(yl$tiles, list(list(name = project_name,
-     url = project_url,
-     img = file.path("logos", basename(outpath)),
-     tags = project_tag)))
+purrr::map_chr(yl$tiles, "name")
+
+with(py$open("data/links.yml", "r") %as% file, {
+ test <- pyyaml$load(file)
+})
+
+test <- pyyaml$load("data/links.yml")
+
+yl <- c(yl$tiles, list(list(
+  name = project_name,
+  url = project_url,
+  img = file.path("logos", basename(outpath)),
+  tags = project_tag)))
 yaml::write_yaml(yl, "data/links.yml")
 
 # x <- yaml.load("speeds: [10mbps, 100mbps]")
