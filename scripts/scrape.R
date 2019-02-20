2752a6ec7ab559123099d437c3cc43ba6408ebc0
jsta <stachel2@msu.edu>
2019-02-20 11:08:59 -0500

    combine cran and non-cran projects

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -1,11 +1,28 @@
-library(pkgsearch)
 library(ghrecipes)
 library(dplyr)
+library(tidyr)
+library(stringr)
 
-r_projects_cran <- pkg_search("Stachelek")
+projects_cran_raw <- tools::CRAN_package_db()
+projects_cran     <- projects_cran_raw[,!duplicated(names(projects_cran_raw))] %>%
+  dplyr::select(Package, Author, Title, URL) %>%
+  dplyr::filter(stringr::str_detect(Author, "Stachelek"))
 
-test <- ghrecipes::get_repos("jsta") %>%
-  data.frame() %>%
-  arrange(test, desc(totalCount))
+projects_gh_raw <- ghrecipes::get_repos("jsta") %>%
+  data.frame() 
+projects_gh     <-  projects_gh_raw %>%
+  mutate(stargazers_count = projects_gh_raw$stargazers_count$totalCount) %>%
+  arrange(desc(stargazers_count)) %>%
+  filter(!is_archived) %>%
+  filter(!is_fork) %>%
+  filter(stargazers_count > 0) %>%
+  mutate(URL = paste0("https://github.com/", name)) %>%
+  rename(Title = description) %>%
+  separate(name, c("accnt", "Package"), sep = "/")
 
-
+res <- bind_rows(projects_cran, projects_gh) %>%
+  select(names(projects_cran)) %>%
+  filter(!duplicated(Package)) %>%
+  filter(!stringr::str_detect(Title, "Reviews")) %>%
+  filter(!stringr::str_detect(Package, c(".io$"))) %>%
+  filter(!stringr::str_detect(Package, c("Notes$")))
