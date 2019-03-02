b7dfd455f01d1ed42af3f819afdf48f8365079f8
jsta <stachel2@msu.edu>
2019-03-02 09:30:55 -0500

    cleanup svg icons

--- scripts/svg.R
+++ scripts/svg.R
@@ -13,6 +13,7 @@ library(magrittr)
 suppressMessages(library(magick))
 library(rsvg)
 library(hexSticker)
+library(ggplot2)
 
 get_cex <- function(x, base = 10, scale = 4){
   if(x > base){
@@ -22,18 +23,37 @@ get_cex <- function(x, base = 10, scale = 4){
   }
 }
 
-if(!file.exists(outpath)){
-  
-  # create svg of project_name
-  svglite(outpath)
-  plot.new()
-  text(0.5, 0.5, project_name, cex = get_cex(nchar(project_name), 9.2, 3.8))
-  dev.off()
-  
-  image_read_svg(outpath) %>%
-    image_trim() %>%
-    image_transparent(color = "white") %>%
-    image_write(outpath, format = "svg")
+make_svg_base <- function(outpath, project_name, overwrite = FALSE){
+  if(!file.exists(outpath) & !overwrite){
+    svglite(outpath, bg = "transparent")
+    par_original <- par()
+    par(bg = NA)
+    plot.new()
+    text(0.5, 0.5, project_name, cex = get_cex(nchar(project_name), 9.2, 3.8))
+    dev.off()
+    par(par_original)
+    
+    image_read_svg(outpath) %>%
+      image_trim() %>%
+      image_write(outpath, format = "svg")
+  }
+}
+
+make_svg_base(outpath, project_name)
+
+make_svg_ggplot <- function(outpath, project_name){
+  # if(!file.exists(outpath)){
+    svglite::svglite(outpath)
+    ggplot(data = data.frame(x = 0, y = 0, text = project_name)) + 
+      geom_text(aes(x = x, y = y, label = text), size = 12) +
+      cowplot::theme_nothing()
+    dev.off()
+    
+    image_read_svg(outpath) %>%
+       image_trim() %>%
+    #   # image_transparent(color = "white") %>%
+       image_write(outpath, format = "svg")
+  # }
 }
 
 # sticker(outpath,
